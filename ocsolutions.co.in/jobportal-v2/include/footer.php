8605e26b2c920e43dd3fe9fd35af3e95ce88a218
Behram Khattak <81041662+Behram-Khattak@users.noreply.github.com>
2021-11-08 09:43:22 -0800

    some changes
    separating the files.

---
+++ ocsolutions.co.in/jobportal-v2/include/footer.php
@@ -0,0 +1,113 @@
+<!-- Footer4 start here -->
+<link href="../../cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<!-- static code start here -->
+<div class="intro">
+	<div class="way-success"></div>
+	<div class="caption">
+		<div class="way-inner">
+			<h3 class="heading_thrd">WAY TO SUCCESS</h3>
+		</div>
+	</div>
+	<span class="overlay">
+		<svg viewBox="0 0 500 250" enable-background="new 0 0 500 250" xml:space="preserve" preserveAspectRatio="none">
+		<path fill="#282b4a" d="M 250 180.5 c -53.85 0 -135.344 -30.044 -250 -165.633 V 262 h 500 V 15.867 C 405.344 180.456 280.85 180.5 250 180.5 Z"></path>
+		</svg>
+	</span>
+</div>
+<!-- static code end here -->
+<footer class="footer4">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-6 col-lg-4 col-xs-12 matter">
+								  <a href="index9328.html?route=common/home"><img src="image/catalog/footer-logo.png"  class="img-responsive" alt="logo" title="logo"/></a>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore accumsan lacus vel facilisis psum primis in faucibus orci luctus et ultr amet tristique erosam.</p>
+			</div>
+	 
+			<div class="col-sm-12 col-md-6 col-lg-5 col-xs-12">
+				<div class="row">
+										<div class="col-sm-6 col-md-5 col-xs-12">
+						<h5>Useful Links</h5> 
+						<ul class="list-unstyled">
+														<li><a href="index8816.html?route=information/information&amp;information_id=4"> About Us</a></li>
+														<li><a href="index99e4.html?route=information/information&amp;information_id=5"> Terms &amp; Conditions</a></li>
+																					<li>
+								<a href="index2724.html?route=information/contact">
+								Contact Us</a>
+							</li>
+														<li>
+								<a href="http://ocsolutions.co.in/jobportal/index.php?route=tmdblog/allblogcategory">
+								News &amp;  Blog</a>
+							</li>
+														<li>
+								<a href="http://ocsolutions.co.in/jobportal/">
+								Our featured</a>
+							</li>
+													</ul> 
+					</div>	
+										<div class="col-sm-6 col-md-7 col-xs-12 contact">
+						<h5>Contact Us</h5>				
+						<ul class="list-unstyled">
+														<li>
+								<a href="javascript:void(0)"><i class="fa fa-map-marker" aria-hidden="true"></i>456, tredy Road, New york
+USA, MD 210093</a>
+							</li>
+																					<li>
+								<a href="javascript:void(0)" class="f_rubik"><i class="fa fa-phone" aria-hidden="true"></i> +61-5869259325</a>
+							</li>
+																					<li>
+								<a href="javascript:void(0)"><i class="fa fa-envelope" aria-hidden="true"></i>hr@gmail.com</a>
+							</li>
+													</ul>	
+					</div>
+				</div>
+			</div>
+	
+			<div class="col-sm-12 col-md-12 col-lg-3 col-xs-12 subscribe">
+				<h5>Get In Touch</h5>
+				<form action="https://themultimediadesigner.us5.list-manage.com/subscribe/post?u=51c1d0d6e4042dfc6c59a8231&amp;id=0f05c71f05" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+					<div class="form-group">
+						<div class="input-group">
+							<div class="mc-field-group">
+							   <input value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="Enter your Email Address" aria-required="true" type="email">
+							</div>
+							<div class="input-group-btn">
+							  <button id="mc-embedded-subscribe" class="btn btn-default btn-lg" type="submit"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+							</div>
+							<div id="mce-responses" class="clear">
+								<div class="response alert alert-danger" id="mce-error-response" style="display:none"></div>
+								<div class="response alert alert-success" id="mce-success-response" style="display:none"></div>
+							</div>
+							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input name="b_51c1d0d6e4042dfc6c59a8231_0f05c71f05" tabindex="-1" value="" type="text"></div>
+						</div>
+					</div>
+				</form>
+
+				<ul class="list-inline socialicon">
+										  <li>
+						<a href="http://www.facebook.com/" target="new"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					  </li>
+					  					  
+					  					  <li>
+						<a href="http://www.twitter.com/" target="new"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					  </li>
+					  					  
+					  					  <li>
+						<a href="http://www.instagram.com/" target="new"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					  </li>
+					  					  
+					   					  <li>
+						<a href="http://www.youtube.com/" target="new"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+					  </li>
+					  					  
+					  					  <li class="hide">
+						<a href="http://www.google.com/" target="new"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+					  </li>
+					  					  					  <li class="hide">
+						<a href="http://www.linkedin.com/" target="new"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+					  </li>
+									</ul>
+			</div>	
+		</div>
+	</div>
+</footer>
+<!-- Footer4 end here -->
---
+++ ocsolutions.co.in/jobportal-v2/include/head.php
@@ -0,0 +1,32 @@
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Jobportal</title>
+<base  />
+<meta name="description" content="Jobportal" />
+<meta name="keywords" content= "Jobportal" />
+<script src="catalog/view/javascript/jquery/jquery-2.1.1.min.js" type="text/javascript"></script>
+<link href="catalog/view/javascript/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+<script src="catalog/view/javascript/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<link href="catalog/view/javascript/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<!-- font -->
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500;600;700&amp;display=swap" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css2?family=Rubik&amp;display=swap" rel="stylesheet" type="text/css">
+<!-- stylesheet -->
+<link href="catalog/view/theme/jobportal/stylesheet/stylesheet.css" rel="stylesheet">
+<link href="catalog/view/theme/jobportal/stylesheet/responsive.css" rel="stylesheet">
+<link href="catalog/view/theme/jobportal/stylesheet/tmdlatestblog.css" rel="stylesheet">
+<!--mega menu file-->
+<link href="catalog/view/theme/jobportal/stylesheet/megaheader.css" rel="stylesheet">			
+<script type="text/javascript" src="catalog/view/theme/jobportal/megamenu/megamenu.js"></script>
+<link rel="stylesheet" type="text/css" href="catalog/view/theme/jobportal/megamenu/menu.css">
+<script type="text/javascript">
+		var responsive_design = 'yes';
+</script>
+<!--mega menu file-->
+<link href="catalog/view/javascript/jquery/owl-carousel/owl.carousel.css" type="text/css" rel="stylesheet" media="screen" />
+<script src="catalog/view/javascript/common.js" type="text/javascript"></script>
+<link href="image/catalog/logo.png" rel="icon" />
+<script src="catalog/view/javascript/jquery/owl-carousel/owl.carousel.min.js" type="text/javascript"></script>
+</head>
